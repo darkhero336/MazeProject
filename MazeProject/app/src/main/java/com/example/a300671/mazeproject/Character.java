db42577f6ac5c21dd4d75b1c7cec337f5751c220
arneson205037 <35339062+arneson205037@users.noreply.github.com>
2019-01-23 10:47:50 -0500

    Update Character.java

--- MazeProject/app/src/main/java/com/example/a300671/mazeproject/Character.java
+++ MazeProject/app/src/main/java/com/example/a300671/mazeproject/Character.java
@@ -3,27 +3,24 @@ package com.example.a300671.mazeproject;
 public class Character
 {
 
-    float x;
-    float y;
-    float oldx; //these are for covering up the player's last position
-    float oldy;
+    float x;  // lateral location of the player character, not scaled to fit the screen
+    float y;  // vertical location of the characer, not scaled to fit the screen
+    float oldx; // these are for covering up the player's last position
+    float oldy; // ^
     int direction;  //0 (right), 1 (up), 2 (left), 3 (down)
-    
-    boolean badx; //used for LAST version of collision detection that ended up sucking too much processing power and not working
-    boolean bady;
 
-    boolean win;
+    boolean win; // used to judge whether or not the character is in win-state
 
     public Character() {
         x = 20f; //These x and y values work on any sized screen b/c when we draw the character they are multiplied by DemoView.stretchValue
         y = 20f;
         oldx = 20f;
         oldy = 20f;
-        direction = 3;
+        direction = 3; // initialized to be in the top left, facing down
         win = false;
     }
 
-    public void resetCharacter() {
+    public void resetCharacter() {  // resets the character to the top left of screen
         x = 20f;
         y = 20f;
         oldx = 20f;
@@ -31,22 +28,22 @@ public class Character
         direction = 3;
     }
 
-    public boolean canMoveForward()
+    public boolean canMoveForward()  // a step in the process of collision detection - checks to see if, based on where the player is and where the player is facing (fields of class, not parameters of method), the player can or cannot move forward
     {
         int tempX = (int) x;
         int tempY = (int) y;
         
-        if (direction == 0) {
-            if(com.example.a205037.maze.Maze.checkCollision((tempX + 20),tempY))
-            {
-                return false;
+        if (direction == 0) { // if the player is facing right
+            if(com.example.a205037.maze.Maze.checkCollision((tempX + 20),tempY)) //checks to see if there is wall-collision in the space in front of the character
+            {                                                                    // 20 is the move rate, the program is hard-wired to work with 20 - think of the maze as a board of spaces the character can be at, each space is 20 units away from the adjacent space
+                return false; // if the collision in the next space is true, then it is false that the player can move forward
             }
             else
             {
-                return true;
+                return true; // if the collision in the next space is false, then it is true that the player can move forward
             }
         }
-        else if (direction == 1) {
+        else if (direction == 1) { // up
             if(com.example.a205037.maze.Maze.checkCollision(tempX, (tempY - 20)))
             {
                 return false;
@@ -56,7 +53,7 @@ public class Character
                 return true;
             }
         }
-        else if (direction == 2) {
+        else if (direction == 2) {  // left
             if(com.example.a205037.maze.Maze.checkCollision((tempX - 20), tempY))
             {
                 return false;
@@ -66,7 +63,7 @@ public class Character
                 return true;
             }
         }
-        else if (direction == 3) {
+        else if (direction == 3) {  // down
             if (com.example.a205037.maze.Maze.checkWin(tempX, (tempY + 20)))
             {
                 win = true;
@@ -84,11 +81,11 @@ public class Character
         else
         {
             System.out.println("Logical infeasibility in canMoveForward"); //Derek added these, idk why
-            return false;
+            return false;                                                  // Derek added these because it is good style
         }
     }
     
-    public boolean canMoveBackward()
+    public boolean canMoveBackward() // Dropped player movement mechanic, it was working to move the character back but wasn't working with collision detection
     {
         int tempX = (int) x;
         int tempY = (int) y;
@@ -144,7 +141,7 @@ public class Character
 
         if (direction == 0) {
             oldx = x;
-            x += 20;                   // make a "speed" variable to replace magic "20" ?
+            x += 20;                   // make a "speed" variable to replace magic "20" ?  -- EDIT: the program is designed as a board, with spaces' center points being 20 units apart
         } else if (direction == 1) {
             oldy = y;
             y -= 20;
@@ -164,7 +161,7 @@ public class Character
 
     }
     
-    public void moveBackward() {
+    public void moveBackward() { // abandoned mechanic, see "canMoveForward()" method
 
 
         if (direction == 0) {
@@ -184,13 +181,13 @@ public class Character
 
 
 
-    public void turnLeft() {
+    public void turnLeft() { // turns the character counter-clockwise
         oldx = x;
         oldy = y;
         direction = (direction + 1) % 4;
     }
     
-    public void turnRight() {
+    public void turnRight() { // turns the character clockwise
         oldx = x;
         oldy = y;
 
@@ -200,32 +197,32 @@ public class Character
             direction = 3;
     }
 
-    public float getX()
+    public float getX() // accessor method to get character's x, though in this version of the program the variable is not private
     {
         return x;
     }
 
-    public float getY()
+    public float getY() // accessor
     {
         return y;
     }
 
-    public float getOldx()
+    public float getOldx() // accessor
     {
         return oldx;
     }
 
-    public float getOldy()
+    public float getOldy() // accessor
     {
         return oldy;
     }
 
-    public int getDirection()
+    public int getDirection() // accessor
     {
         return direction;
     }
 
-    public boolean getWin() {
+    public boolean getWin() { // accessor
         return win;
     }
 }
